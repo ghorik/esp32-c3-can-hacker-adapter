41888ab2333832a934fc118a90542bae9e8839ae
Ghorik <evgeny.pospelov@maliningroup.com>
2024-07-25 11:00:13 +0500

    Исправлкны номера пинов для подключения к плате трансивера

--- include/project_config.h
+++ include/project_config.h
@@ -2,7 +2,7 @@
 #ifndef __PROJECT_CONFIG_H
 #define __PROJECT_CONFIG_H
 
-#define CAN_CTX_PIN             GPIO_NUM_6
-#define CAN_CRX_PIN             GPIO_NUM_5
+#define CAN_CTX_PIN             GPIO_NUM_5
+#define CAN_CRX_PIN             GPIO_NUM_6
 
 #endif
